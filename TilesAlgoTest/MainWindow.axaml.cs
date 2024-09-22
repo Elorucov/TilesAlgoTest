e5bfd041ffbec4baa887f5f05d82a55207b3fd1c
Elorucov <elorucov@live.ru>
2024-09-22 15:40:06 +0400

    Add project files.

---
+++ TilesAlgoTest/MainWindow.axaml.cs
@@ -0,0 +1,193 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Layout;
+using Avalonia.Media;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace TilesAlgoTest {
+    public partial class MainWindow : Window {
+        int column = 6;
+        double tilesLayoutWidth;
+        List<Tile> SampleTiles;
+
+        public MainWindow() {
+            InitializeComponent();
+
+            tilesLayoutWidth = TilesLayout.Width;
+            SampleTiles = Tile.GetSampleTiles();
+            Test();
+        }
+
+        private void CheckTiles() {
+            CheckIsOutOfBounds();
+            CheckVerticalIntersect();
+            FixEmptyRows();
+        }
+
+        private void CheckIsOutOfBounds() {
+            foreach (Tile tile in SampleTiles) {
+                Rect trect = tile.Rect;
+                if (trect.Right > column) {
+                    tile.Column -= Convert.ToInt32(trect.Right - column);
+                    tile.Row += 1;
+                }
+            }
+        }
+
+        private void CheckVerticalIntersect() {
+            int extra = 0;
+            int i = 0;
+            foreach (Tile tile in SampleTiles) {
+                if (i == 0) {
+                    i += 1;
+                    continue;
+                }
+                if (extra > 0) tile.Row += extra;
+
+                Rect trect = tile.Rect;
+                List<Rect> rects = SampleTiles.Take(i).Select(t => t.Rect).ToList();
+                foreach (Rect rect in CollectionsMarshal.AsSpan(rects)) {
+                    var intersect = trect.Intersect(rect);
+                    if (intersect.Height >= 1) { // If tile (trect) have a collision, move it under (rect)
+                        int add = Convert.ToInt32(rect.Height + rect.Top - trect.Top);
+                        tile.Row += add;
+                        extra += add;
+                    }
+                }
+
+                i += 1;
+            }
+        }
+
+        // Maybe there is a more optimized version?
+        private void FixEmptyRows() {
+            int rows = Convert.ToInt32(SampleTiles.Max(t => t.Rect.Bottom));
+            //int emptyRows = 0;
+            //for (int row = 0; row < rows; row++) {
+            //    bool rowOccupied = false;
+            //    foreach (Tile tile in SampleTiles) {
+            //        if (tile.Rect.Top <= row && tile.Rect.Bottom <= row) { 
+            //            rowOccupied = true;
+            //            break;
+            //        }
+            //    }
+            //    if (!rowOccupied) emptyRows++;
+            //}
+
+            List<int> nonEmptyRows = new List<int>();
+            List<int> emptyRows = new List<int>();
+
+            foreach (Tile tile in SampleTiles) {
+                var rect = tile.Rect;
+                for (int row = Convert.ToInt32(rect.Top); row <= Convert.ToInt32(rect.Bottom - 1); row++) {
+                    if (!nonEmptyRows.Contains(row)) nonEmptyRows.Add(row);
+                }
+            }
+
+            for (int i = 0; i < rows; i++) {
+                if (!nonEmptyRows.Contains(i)) emptyRows.Add(i);
+            }
+
+            foreach (Tile tile in SampleTiles) {
+                var move = emptyRows.Where(r => r < tile.Row).Count();
+                if (move > 0) tile.Row = tile.Row - move;
+            }
+        }
+
+        private void Test() {
+            CheckTiles();
+
+            double smallTileSize = tilesLayoutWidth / column;
+
+            foreach (var tile in SampleTiles) {
+                Rect rect = tile.Rect;
+                double width = smallTileSize * rect.Width;
+                double height = smallTileSize * rect.Height;
+                double top = smallTileSize * tile.Row;
+                double left = smallTileSize * tile.Column;
+
+                Control c = TilesLayout.Children.Where(c => (c.DataContext as Tile)?.Id == tile.Id).FirstOrDefault();
+                if (c != null) {
+                    c.Width = width;
+                    c.Height = height;
+                    Canvas.SetLeft(c, left);
+                    Canvas.SetTop(c, top);
+                } else {
+                    Border b = new Border {
+                        DataContext = tile,
+                        Padding = new Thickness(4),
+                        Width = width,
+                        Height = height,
+                        Child = new Border {
+                            Background = new SolidColorBrush(Colors.MediumPurple),
+                            HorizontalAlignment = HorizontalAlignment.Stretch,
+                            VerticalAlignment = VerticalAlignment.Stretch,
+                            Child = new TextBlock {
+                                VerticalAlignment = VerticalAlignment.Center,
+                                TextAlignment = TextAlignment.Center,
+                                Text = $"{tile.Id}: {tile.Name}",
+                                Foreground = new SolidColorBrush(Colors.White)
+                            }
+                        }
+                    };
+                    Canvas.SetLeft(b, left);
+                    Canvas.SetTop(b, top);
+
+                    TilesLayout.Children.Add(b);
+
+                    b.ContextRequested += TileContextRequested;
+                }
+            }
+        }
+
+        private void TileContextRequested(object sender, ContextRequestedEventArgs e) {
+            Border b = sender as Border;
+            Tile t = b.DataContext as Tile;
+
+            MenuItem smaler = new MenuItem { Header = "Smaller" };
+            MenuItem larger = new MenuItem { Header = "Larger" };
+            MenuItem remove = new MenuItem { Header = "Remove" };
+
+            smaler.Click += (a, c) => Resize(t, false);
+            larger.Click += (a, c) => Resize(t, true);
+            remove.Click += (a, c) => Remove(t, b);
+
+            MenuFlyout mf = new MenuFlyout();
+            mf.Items.Add(smaler);
+            mf.Items.Add(larger);
+            mf.Items.Add(remove);
+
+            mf.ShowAt(b, true);
+        }
+
+        private void Resize(Tile tile, bool enlarge) {
+            if (tile.Size == TileSize.Small) {
+                if (enlarge) tile.Size = TileSize.Medium;
+            } else if (tile.Size == TileSize.Medium) {
+                tile.Size = enlarge ? TileSize.Wide : TileSize.Small;
+            }
+            else if (tile.Size == TileSize.Wide) {
+                tile.Size = enlarge ? TileSize.Large : TileSize.Medium;
+            }
+            else if (tile.Size == TileSize.Large) {
+                if (!enlarge) tile.Size = TileSize.Wide;
+            }
+
+            Test();
+        }
+
+        private void Remove(Tile tile, Border border) {
+            SampleTiles.Remove(tile);
+            TilesLayout.Children.Remove(border);
+            Test();
+        }
+
+        private void Button_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e) {
+            SampleTiles = Tile.GetSampleTiles();
+            Test();
+        }
+    }
+}
---
+++ TilesAlgoTest/Tile.cs
@@ -0,0 +1,66 @@
+﻿using Avalonia;
+using System.Collections.Generic;
+
+namespace TilesAlgoTest {
+    //                       1x1,   2x2,    4x2,  3x3
+    internal enum TileSize { Small, Medium, Wide, Large }
+
+    internal class Tile {
+        public int Id { get; private set; }
+        public int Row {  get; set; }
+        public int Column { get; set; }
+        public TileSize Size { get; set; }
+        public string Name { get; set; }
+        public Rect Rect { get { return ToRect(); } }
+
+        internal Tile(int id, int row, int col, TileSize size, string name) {
+            Id = id;
+            Row = row;
+            Column = col;
+            Size = size;
+            Name = name;
+        }
+
+        private Rect ToRect() {
+            int w = 1;
+            int h = 1;
+
+            switch (Size) {
+                case TileSize.Medium: w = 2; h = 2; break;
+                case TileSize.Wide: w = 4; h = 2; break;
+                case TileSize.Large: w = 3; h = 3; break;
+            }
+
+            return new Rect(Column, Row, w, h);
+        }
+
+        internal static List<Tile> GetSampleTiles() {
+            return new List<Tile> { 
+                new Tile(1, 0, 0, TileSize.Small, "A"),
+                new Tile(2, 0, 1, TileSize.Small, "B"),
+                new Tile(3, 0, 2, TileSize.Small, "C"),
+                new Tile(4, 0, 3, TileSize.Small, "D"),
+                new Tile(5, 0, 4, TileSize.Small, "E"),
+                new Tile(6, 0, 5, TileSize.Small, "F"),
+
+                new Tile(7, 1, 0, TileSize.Wide, "G"),
+                new Tile(8, 1, 4, TileSize.Small, "H"),
+                new Tile(9, 1, 5, TileSize.Small, "I"),
+                new Tile(10, 2, 4, TileSize.Small, "J"),
+                new Tile(11, 2, 5, TileSize.Small, "K"),
+
+                new Tile(12, 3, 0, TileSize.Small, "L"),
+                new Tile(13, 3, 1, TileSize.Small, "M"),
+                new Tile(14, 3, 2, TileSize.Medium, "N"),
+                new Tile(15, 3, 4, TileSize.Medium, "O"),
+                new Tile(16, 4, 0, TileSize.Small, "P"),
+                new Tile(17, 4, 1, TileSize.Small, "Q"),
+
+                new Tile(18, 5, 0, TileSize.Medium, "R"),
+                new Tile(19, 5, 2, TileSize.Large, "S"),
+                new Tile(20, 7, 0, TileSize.Small, "T"),
+                new Tile(21, 7, 1, TileSize.Small, "U"),
+            };
+        }
+    }
+}
